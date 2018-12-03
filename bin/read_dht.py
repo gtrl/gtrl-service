f2966b5e5837b330a674400c010a5c782d53a657
tong <tong@disktree.net>
2018-12-03 16:25:57 +0100

    Endless sensor read to avoid initializing

--- bin/read_dht.py
+++ bin/read_dht.py
@@ -4,16 +4,12 @@ import sys
 import struct
 import Adafruit_DHT
 
-args = sys.argv[1:]
-pin = args[0]
-
 sensor = Adafruit_DHT.DHT22
-humidity, temperature = Adafruit_DHT.read(sensor, pin)
-# humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-if humidity is not None and temperature is not None:
-    sys.stdout.buffer.write(bytearray(struct.pack("f", temperature)))
-    sys.stdout.buffer.write(bytearray(struct.pack("f", humidity)))
-    exit(0)
-else:
-    exit(1)
+while True:
+    pin = int.from_bytes(sys.stdin.buffer.raw.read(1), byteorder='little')
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    if humidity is not None and temperature is not None:
+        sys.stdout.buffer.write(bytearray(struct.pack("f", temperature)))
+        sys.stdout.buffer.write(bytearray(struct.pack("f", humidity)))
+        sys.stdout.flush()
