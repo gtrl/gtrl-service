ede8aada879a786832890c126b54c2d4e4003a9e
tong <tong@disktree.net>
2018-12-04 09:53:25 +0100

    Return error code when sensor read fails

--- bin/read_dht.py
+++ bin/read_dht.py
@@ -8,8 +8,10 @@ sensor = Adafruit_DHT.DHT22
 
 while True:
     pin = int.from_bytes(sys.stdin.buffer.raw.read(1), byteorder='little')
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    humidity, temperature = Adafruit_DHT.read_(sensor, pin)
     if humidity is not None and temperature is not None:
         sys.stdout.buffer.write(bytearray(struct.pack("f", temperature)))
         sys.stdout.buffer.write(bytearray(struct.pack("f", humidity)))
-        sys.stdout.flush()
+    else:
+        sys.stdout.buffer.write(bytearray(struct.pack("B", 0)))
+    sys.stdout.flush()
