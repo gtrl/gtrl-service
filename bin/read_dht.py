6f67fc3571ec02dc71898a17821c05faf7d7a5db
tong <tong@disktree.net>
2018-12-04 10:13:13 +0100

    Set sensor type

--- bin/read_dht.py
+++ bin/read_dht.py
@@ -4,11 +4,10 @@ import sys
 import struct
 import Adafruit_DHT
 
-sensor = Adafruit_DHT.DHT22
-
 while True:
     pin = int.from_bytes(sys.stdin.buffer.raw.read(1), byteorder='little')
-    humidity, temperature = Adafruit_DHT.read_(sensor, pin)
+    type = int.from_bytes(sys.stdin.buffer.raw.read(1), byteorder='little')
+    humidity, temperature = Adafruit_DHT.read(type, pin)
     if humidity is not None and temperature is not None:
         sys.stdout.buffer.write(bytearray(struct.pack("f", temperature)))
         sys.stdout.buffer.write(bytearray(struct.pack("f", humidity)))
