cb7430cfe73a223c68272e42f4f7ead79b97bac8
tong <tong@disktree.net>
2018-12-05 09:31:31 +0100

    Retry sensor read

--- bin/read_dht.py
+++ bin/read_dht.py
@@ -7,7 +7,7 @@ import Adafruit_DHT
 while True:
     pin = int.from_bytes(sys.stdin.buffer.raw.read(1), byteorder='little')
     type = int.from_bytes(sys.stdin.buffer.raw.read(1), byteorder='little')
-    humidity, temperature = Adafruit_DHT.read(type, pin)
+    humidity, temperature = Adafruit_DHT.read_retry(type, pin, 10, 1)
     if humidity is not None and temperature is not None:
         sys.stdout.buffer.write(bytearray(struct.pack("f", temperature)))
         sys.stdout.buffer.write(bytearray(struct.pack("f", humidity)))
